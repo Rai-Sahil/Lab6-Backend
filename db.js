2568687ad42ef123be9df6c833f644bfd20cd482
Sahil Rai <Raisahil580@gmail.com>
2023-11-05 19:52:20 -0800

    first commit

---
+++ db.js
@@ -0,0 +1,160 @@
+const mysql = require('mysql');
+const databaseName = "dictionary";
+const tableName = "words";
+const languageTableName = "language";
+// SQL queries
+const createTableQuery = `CREATE TABLE IF NOT EXISTS ${tableName} (word VARCHAR(255), definition VARCHAR(255), wordLanguage VARCHAR(255), definitionLanguage VARCHAR(255))`;
+const createTableLanguageQuery = `CREATE TABLE IF NOT EXISTS ${languageTableName} (language VARCHAR(255))`;
+const insertLanguageTableQuery = `INSERT INTO ${languageTableName} (language) VALUES ?`;
+const selectLanguageTableQuery = `SELECT language FROM ${languageTableName}`;
+const insertWordQuery = `INSERT INTO ${tableName} (word, definition, wordLanguage, definitionLanguage) VALUES (?, ?, ?, ?)`;
+const updateWordQuery = `UPDATE ${tableName} SET definition = ?, wordLanguage = ?, definitionLanguage = ? WHERE word = ?`;
+const deleteWordQuery = `DELETE FROM ${tableName} WHERE word = ?`;
+const selectWordQuery = `SELECT * FROM ${tableName} WHERE word = ?`;
+const english = "English";
+const chinese = "Chinese";
+const arabic = "Arabic";
+const resultLog = "results are: ";
+const log1 = "All languages already exist in the database. No languages added.";
+const log2 = "Word inserted successfully";
+const tableLog = "Table created";
+
+var con = mysql.createConnection({
+    host: "sql3.freesqldatabase.com",
+    user: "sql3659563",
+    password: "d8pH6dC9T6",
+    database: "sql3659563",
+    port: 3306
+})
+
+function createTable() {
+    con.query(createTableQuery, function (err, result) {
+        if (err) throw err;
+        console.log(tableLog);
+    });
+}
+
+function createTableLanguage() {
+    return new Promise((resolve, reject) => {
+        con.query(createTableLanguageQuery, async (err, result) => {
+            console.log(resultLog + JSON.stringify(result));
+            if (err) reject(err);
+            else resolve(result);
+        });
+    })
+}
+
+function addLanguages() {
+    return new Promise((resolve, reject) => {
+        const languages = [english, chinese, arabic];
+
+        con.query(selectLanguageTableQuery, function (err, result) {
+            if (err) throw err;
+
+            const existingLanguages = result.map(row => row.language);
+            const newLanguages = languages.filter(language => !existingLanguages.includes(language));
+
+            if (newLanguages.length === 0) {
+                console.log(log1);
+                resolve();
+            } else {
+                const values = newLanguages.map(language => [language]);
+
+                con.query(insertLanguageTableQuery, [values], function (err, result) {
+                    if (err) reject(err);
+                    resolve(result);
+                });
+            }
+        });
+    });
+}
+
+
+exports.wordExists = function (word) {
+    return new Promise((resolve, reject) => {
+        createTable();
+        const query = selectWordQuery;
+
+        con.query(query, [word], function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(resultLog + JSON.stringify(result));
+                resolve(result.length > 0);
+            }
+        });
+    });
+};
+
+exports.insertWord = function (word, definition, wordLanguage, definitionLanguage) {
+    createTable();
+    const sql = insertWordQuery;
+    const values = [word, definition, wordLanguage, definitionLanguage];
+
+    con.query(sql, values, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(log2);
+        }
+    });
+}
+
+exports.updateWord = function (word, definition, wordLanguage, definitionLanguage) {
+    createTable();
+    return new Promise((resolve, reject) => {
+        const query = updateWordQuery;
+        const values = [definition, wordLanguage, definitionLanguage, word];
+
+        con.query(query, values, function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(resultLog + JSON.stringify(result));
+                resolve(result);
+            }
+        });
+    });
+}
+
+exports.deleteWord = function (word) {
+    createTable();
+    return new Promise((resolve, reject) => {
+        const query = deleteWordQuery;
+
+        con.query(query, [word], function (err, res) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+}
+
+exports.getDefinition = function (word) {
+    createTable();
+    return new Promise((resolve, reject) => {
+        const query = selectWordQuery;
+
+        con.query(query, [word], function (err, res) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    })
+}
+
+exports.getLanguages = async function () {
+    await createTableLanguage();
+    await addLanguages();
+
+    return new Promise((resolve, reject) => {
+        con.query(selectLanguageTableQuery, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+        });
+    });
+};
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./db');
+const definitionRoute = '/api/v1/definition';
+const wordDefinitionRoute = '/api/v1/definition/:word';
+const getLanguageRoute = '/api/v1/languages';
+const serverMsg = "Server is running on port";
+const updateMsg = "Dictionary entry updated successfully";
+const wordExistsMsg = "Word already exists in the dictionary. Do you want to update the definition?";
+const wordNotFoundMsg = "Word not found in the dictionary";
+const update = "update";
+const insert = "inserted";
+const dbCheck = "DB check result is ";
+const app = express();
+const port = 3000;
+let numbeOfRequests = 0;
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// Create a new dictionary entry or update if the word exists
+app.post(definitionRoute, async (req, res) => {
+  const newEntry = req.body;
+
+  if (await db.wordExists(newEntry.word)) {
+    return res.status(200).json({
+      message: wordExistsMsg,
+      word: newEntry.word,
+      action: update,
+    });
+  } else {
+    db.insertWord(newEntry.word, newEntry.definition, newEntry.wordLanguage, newEntry.definitionLanguage)
+    res.status(201).json({ message: updateMsg, entry: newEntry, action: insert, total: ++numbeOfRequests })
+  }
+
+  console.log(dbCheck + db.wordExists(newEntry.word));
+});
+
+
+// Handle the update when the user confirms
+app.patch(wordDefinitionRoute, (req, res) => {
+  const wordToUpdate = req.params.word;
+  const newDefinition = req.body.definition;
+  const newWordLanguage = req.body.wordLanguage;
+  const newDefinitionLanguage = req.body.definitionLanguage;
+
+  if (db.updateWord(wordToUpdate, newDefinition, newWordLanguage, newDefinitionLanguage)) {
+    res.status(200).json({ message: updateMsg, total: ++numbeOfRequests});
+  } else {
+    res.status(404).json({ message: wordNotFoundMsg, total: ++numbeOfRequests });
+  }
+});
+
+
+// Retrieve the definition of a word
+app.get(wordDefinitionRoute, async (req, res) => {
+  const wordToRetrieve = req.params.word;
+  const wordExists = await db.wordExists(wordToRetrieve);
+
+  if (wordExists) {
+    const definition = await db.getDefinition(wordToRetrieve);
+    res.status(200).json({ definition: definition, total: ++numbeOfRequests });
+  } else {
+    res.status(404).json({ message: wordNotFoundMsg, total: ++numbeOfRequests});
+  }
+});
+
+
+// Remove the word and its definition
+app.delete(wordDefinitionRoute, async (req, res) => {
+  const wordToDelete = req.params.word;
+  if (await db.deleteWord(wordToDelete)) {
+    res.status(200).json({ total: ++numbeOfRequests });
+  } else {
+    res.status(404).json({ message: wordNotFoundMsg, total: ++numbeOfRequests });
+  }
+});
+
+// Retrieve supported languages
+app.get(getLanguageRoute , async (req, res) => {
+  // Replace with your list of supported languages
+  const supportedLanguages = await db.getLanguages();
+  res.status(200).json({ supportedLanguages: supportedLanguages });
+});
+
+
+app.listen(port, () => {
+  console.log(serverMsg + port);
+});
